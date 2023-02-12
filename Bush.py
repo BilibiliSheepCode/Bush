4dbfcecc9f5dacc16dc398d6b8ef239950dd3219
BilibiliSheepCode <scode2007@outlook.com>
2023-02-12 11:49:58 +0800

    Added some simple functions

--- Bush.py
+++ Bush.py
@@ -1,8 +1,11 @@
 import sys
 import os
-import numpy as np
 import math
 import time
+import numpy as np
+from PIL import Image
+from scipy import ndimage
+
 
 '''
 #def y_h = sigmoid(w ^ T * x + b) WHILE sigmoid(z) = 1 / (1 + e^(-z)) ; y_h MEANS y_hat
@@ -23,6 +26,22 @@ import time
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+def image2vector(image):
+    return image.reshape(image.shape[0] * image.shape[1] * image.shape[2], 1)
+
+def normalizeRows(x):
+    x = x / np.linalg.norm(x, axis = 1, keepdims = True)
+    return x
+
+def L1(yhat, y):
+    return np.sum(np.abs(y - yhat))
+
+def L2(yhat, y):
+    return np.dot((y - yhat),(y - yhat).T)
 m = 0
 n_x = 2
 dw = np.zeros((n_x, 1))
