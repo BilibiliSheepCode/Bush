ce7d4ad63373b597d7a54be82ee942bf76346a39
BilibiliSheepCode <scode2007@outlook.com>
2023-02-25 21:59:28 +0800

    Copyed some code from AI course.

--- Bush.py
+++ Bush.py
@@ -6,23 +6,6 @@ import numpy as np
 from PIL import Image
 from scipy import ndimage
 
-
-'''
-#def y_h = sigmoid(w ^ T * x + b) WHILE sigmoid(z) = 1 / (1 + e^(-z)) ; y_h MEANS y_hat
-
-#def L(y_h, y) = -(y * log(y_h) + (1 - y) * log(1 - y_h)) AS Loss_Function
-
-#def J(w, b) = (1 / m) * SIGMA(i = 1, m)(L(y_h ^ (i), y ^ (i))) AS Cost_Function
-
-#def Gradient_Descent: {
-    REPEAT {
-        w := w - .alpha. * d J(w, b) / d w ; d EQU partial d AT HERE
-        b := b - .alpha. * d J(w, b) / d b ; d EQU partial d AT HERE
-    }
-}
-
-'''
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -37,11 +20,78 @@ def normalizeRows(x):
     x = x / np.linalg.norm(x, axis = 1, keepdims = True)
     return x
 
+def softmax(x):
+    x_exp = np.exp(x)
+    return x_exp / np.sum(x_exp, axis = 1, keepdims = True)
+
 def L1(yhat, y):
     return np.sum(np.abs(y - yhat))
 
 def L2(yhat, y):
     return np.dot((y - yhat),(y - yhat).T)
-m = 0
-n_x = 2
-dw = np.zeros((n_x, 1))
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim, 1))
+    b = 0
+    return w, b
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+    dw = 1 / m * np.dot(X, (A - Y).T)
+    db = 1 / m * np.sum(A - Y)
+    cost = np.squeeze(cost)
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    for i in range(num_iterations):
+        grads, cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    grads = {"dw": dw,
+             "db": db}
+    return params, grads, costs
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T, X) + b)
+    for i in range(A.shape[1]):
+        if A[0, i] <= 0.5:
+            Y_prediction[0, i] = 0
+        else:
+            Y_prediction[0, i] = 1
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    w, b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    w = parameters["w"]
+    b = parameters["b"]
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    return d
