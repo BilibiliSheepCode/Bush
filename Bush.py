72891e50c87a618ff6ae29c8adb695938f4b83c7
BilibiliSheepCode <scode2007@outlook.com>
2023-03-04 14:46:41 +0800

    Rewriting for some functions

--- Bush.py
+++ Bush.py
@@ -9,10 +9,6 @@ from scipy import ndimage
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x):
-    s = sigmoid(x)
-    return s * (1 - s)
-
 def image2vector(image):
     return image.reshape(image.shape[0] * image.shape[1] * image.shape[2], 1)
 
@@ -24,74 +20,20 @@ def softmax(x):
     x_exp = np.exp(x)
     return x_exp / np.sum(x_exp, axis = 1, keepdims = True)
 
+def tanh(x):
+    ex = np.exp(x)
+    e_x = np.exp(-x)
+    return (ex - e_x) / (ex + e_x)
+
+def ReLU(x):
+    return np.max(0, x)
+
+def Leaky_ReLU(x):
+    return np.max(0.01 * x, x)
+
 def L1(yhat, y):
     return np.sum(np.abs(y - yhat))
 
 def L2(yhat, y):
     return np.dot((y - yhat),(y - yhat).T)
 
-def initialize_with_zeros(dim):
-    w = np.zeros((dim, 1))
-    b = 0
-    return w, b
-
-def propagate(w, b, X, Y):
-    m = X.shape[1]
-    A = sigmoid(np.dot(w.T, X) + b)
-    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
-    dw = 1 / m * np.dot(X, (A - Y).T)
-    db = 1 / m * np.sum(A - Y)
-    cost = np.squeeze(cost)
-    grads = {"dw": dw,
-             "db": db}
-    
-    return grads, cost
-
-def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
-    costs = []
-    for i in range(num_iterations):
-        grads, cost = propagate(w, b, X, Y)
-        dw = grads["dw"]
-        db = grads["db"]
-        w = w - learning_rate * dw
-        b = b - learning_rate * db
-        if i % 100 == 0:
-            costs.append(cost)
-        if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
-    
-    params = {"w": w,
-              "b": b}
-    grads = {"dw": dw,
-             "db": db}
-    return params, grads, costs
-
-def predict(w, b, X):
-    m = X.shape[1]
-    Y_prediction = np.zeros((1,m))
-    w = w.reshape(X.shape[0], 1)
-    A = sigmoid(np.dot(w.T, X) + b)
-    for i in range(A.shape[1]):
-        if A[0, i] <= 0.5:
-            Y_prediction[0, i] = 0
-        else:
-            Y_prediction[0, i] = 1
-    return Y_prediction
-
-def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
-    w, b = initialize_with_zeros(X_train.shape[0])
-    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
-    w = parameters["w"]
-    b = parameters["b"]
-    Y_prediction_test = predict(w, b, X_test)
-    Y_prediction_train = predict(w, b, X_train)
-    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
-    d = {"costs": costs,
-         "Y_prediction_test": Y_prediction_test, 
-         "Y_prediction_train" : Y_prediction_train, 
-         "w" : w, 
-         "b" : b,
-         "learning_rate" : learning_rate,
-         "num_iterations": num_iterations}
-    return d
