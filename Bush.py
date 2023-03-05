ebda2bed6e3dc509b8ceaed0e0ee0ab2fd5f88e1
BilibiliSheepCode <scode2007@outlook.com>
2023-03-05 10:47:21 +0800

    Debuged

--- Bush.py
+++ Bush.py
@@ -1,13 +1,7 @@
-import sys
-import os
-import math
 import time
 import numpy as np
 from PIL import Image
 from scipy import ndimage
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
 
 def image2vector(image):
     return image.reshape(image.shape[0] * image.shape[1] * image.shape[2], 1)
@@ -16,14 +10,15 @@ def normalizeRows(x):
     x = x / np.linalg.norm(x, axis = 1, keepdims = True)
     return x
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
 def softmax(x):
     x_exp = np.exp(x)
     return x_exp / np.sum(x_exp, axis = 1, keepdims = True)
 
 def tanh(x):
-    ex = np.exp(x)
-    e_x = np.exp(-x)
-    return (ex - e_x) / (ex + e_x)
+    return np.tanh(x)
 
 def ReLU(x):
     return np.max(0, x)
@@ -37,3 +32,30 @@ def L1(yhat, y):
 def L2(yhat, y):
     return np.dot((y - yhat),(y - yhat).T)
 
+m = 100000
+alpha = 0.1
+w1 = np.random.randn(4, 2)
+w2 = np.random.randn(1, 4)
+b1 = np.zeros((4, 1))
+b2 = np.zeros((1, 1))
+while True:
+    x_train = np.random.rand(2, m)
+    y_train = np.where(x_train[0] > x_train[1], 0, 1)
+    z1 = np.dot(w1, x_train) + b1
+    a1 = tanh(z1)
+    a2 = sigmoid(np.dot(w2, a1) + b2)
+    dz1 = w2.T * (1 - z1 * z1) * (a2 - y_train)
+    dz2 = (a2 - y_train)
+    dw1 = np.dot(dz1, x_train.T) / m
+    dw2 = np.dot(dz2, a1.T) / m
+    db1 = np.sum(dz1, axis = 1) / m
+    db2 = np.sum(dz2, axis = 1) / m
+    w1 = w1 - alpha * dw1
+    w2 = w2 - alpha * dw2
+    b1 = b1 - alpha * db1.reshape((4, 1))
+    b2 = b2 - alpha * db2.reshape((1, 1))
+    a = float(input())
+    b = float(input())
+    a1 = tanh(np.dot(w1, np.array([a, b]).reshape(2, 1)) + b1)
+    a2 = sigmoid(np.dot(w2, a1) + b2)
+    print(a2)
